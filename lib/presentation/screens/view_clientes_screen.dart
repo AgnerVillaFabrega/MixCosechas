977eed2c901942d2577c9c15797178986a3b882c
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2023-08-22 21:09:53 -0500

    config modify

--- lib/presentation/screens/view_clientes_screen.dart
+++ lib/presentation/screens/view_clientes_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mixcosechas_app/services/firebase_service.dart';
 import '../../model/clientes.dart';
-import 'registration_clients_scren.dart';
 import 'package:mixcosechas_app/presentation/widgets/icon_add_clientes.dart';
 
 class ViewClientScreen extends StatelessWidget {
@@ -24,7 +23,7 @@ class ClientPage extends StatefulWidget {
 class _ClientPageState extends State<ClientPage> {
 
 
- ServiceFirebase _serviceFirebase = ServiceFirebase();
+final ServiceFirebase _serviceFirebase = ServiceFirebase();
 
   List<Cliente> _users = [];
   List<Cliente> _filteredClientes = [];
@@ -43,11 +42,10 @@ class _ClientPageState extends State<ClientPage> {
     });
   }
 
-
   void _filterClientes(String query) {
     List<Cliente> filteredList = _users.where((cliente) {
       return cliente.nombre.toLowerCase().contains(query.toLowerCase()) ||
-             cliente.id.toLowerCase().contains(query.toLowerCase());
+            cliente.id.toLowerCase().contains(query.toLowerCase());
     }).toList();
 
     setState(() {
@@ -93,7 +91,7 @@ class _ClientPageState extends State<ClientPage> {
                 itemBuilder: (context, index) {
                   return ListTile(
                     title: Text(_filteredClientes[index].nombre),
-                    subtitle: Text(_filteredClientes[index].telefono),
+                    subtitle: Text(_filteredClientes[index].id),
                     onTap: () {
                       // Lógica para seleccionar un cliente
                     },
