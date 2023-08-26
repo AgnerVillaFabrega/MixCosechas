8228980aa9d1d45b645955c2404dde60ed92f3e4
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2023-08-25 21:20:30 -0500

    fix: archivo innecesario

--- lib/presentation/screens/view_clientes_screen.dart
+++ lib/presentation/screens/view_clientes_screen.dart
@@ -1,10 +1,37 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mixcosechas_app/presentation/widgets/indicador_circle_progress.dart';
 import '../../model/clientes.dart';
 import 'package:mixcosechas_app/presentation/widgets/icon_add_clientes.dart';
 
+Widget cargarDatos(){
+  return StreamBuilder(
+    stream: FirebaseFirestore.instance.collection('Usuarios').snapshots(),
+    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+      //print(snapshot.connectionState);
+      switch(snapshot.connectionState){
+        case ConnectionState.waiting: 
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        case ConnectionState.active: 
+          if(snapshot.hasError) return const Text('error');
+          return snapshot.data != null? ViewClientScreen(cliente:conversiondelista(snapshot.data!.docs)): const Text('Sin datos');
+        default: return const Text('Presiona el boton para recargar');
+      }
+    }
+  );
+}
+
+List<Cliente> conversiondelista(List registro){
+  List<Cliente> resultado = [];
+  for (var i = 0; i < registro.length; i++) {
+    resultado.add(Cliente(id: registro[i]['Id'], nombre: registro[i]['Nombre'], telefono: registro[i]['Telefono'], correo: registro[i]['Correo'], rol: registro[i]['Rol'], password: registro[i]['Password']));
+  }
+  return resultado;
+}
 
 class ViewClientScreen extends StatefulWidget {
   List<Cliente> cliente = [];
